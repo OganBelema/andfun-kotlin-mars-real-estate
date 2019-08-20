59cf6d1708428277cbff604fad13f41f13d93adb
tamunobelemamiebaka-ogan <belema@softcom.ng>
2019-08-20 09:15:55 +0100

    Add loading indicator and error image to glide

--- app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -21,12 +21,18 @@ import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 @BindingAdapter("imageUrl")
 fun ImageView.bindImage(imageUrl: String?){
     imageUrl?.let {
         val imageUri = it.toUri().buildUpon().scheme("https").build()
-        Glide.with(this.context).load(imageUri).into(this)
+        Glide.with(this.context)
+                .load(imageUri)
+                .apply(RequestOptions()
+                        .placeholder(R.drawable.loading_animation)
+                        .error(R.drawable.ic_broken_image))
+                .into(this)
     }
 }
 
